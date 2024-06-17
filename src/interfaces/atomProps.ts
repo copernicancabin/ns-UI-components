c6cfcb27dc7342a4ed17a4f3e83e9bb5dd0c8d19
Himanshu Savita <naptunesailor@gmail.com>
2024-06-17 14:36:23 +0530

    [TAS-27] Adding first improvements for Column component

---
+++ src/interfaces/atomProps.ts
@@ -0,0 +1,10 @@
+import { columnOffsetType, columnSizeType } from '../types/styleTypes'
+
+export interface ButtonProps {
+  text?: string
+}
+
+export interface ColumnProps {
+  size: columnSizeType
+  offset: columnOffsetType
+}
